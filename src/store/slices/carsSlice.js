fd247e48a46f4c27010f2310655b9492f8a04b00
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-05-18 17:57:37 +0900

    feat: create carSlice and formSlice. implement  states and actions

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    cars: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      // Assumption: action.payload === { name: 'ab', const: 140 }
+      state.cars.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(), // redux random id generator
+      });
+    },
+    removeCar(state, action) {
+      // Assumption: action.paylod === the id of the car we want to remove
+      const updated = state.cars.filter((car) => {
+        return car.id !== action.payload;
+      });
+      state.cars = updated;
+    },
+  },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
