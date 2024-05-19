1992ef719a70526e1019c18410c829466dd843b2
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-05-19 10:34:24 +0900

    feat: display cars list. Refactor car slice state "cars" => "data" for clarity accessing state

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
 function CarList() {
-  return <div>CarList</div>;
+  const { cars } = useSelector((state) => {
+    return state.cars;
+  });
+
+  return (
+    <div>
+      {cars.map((car) => (
+        <div>{car.name} </div>
+      ))}
+    </div>
+  );
 }
 
 export default CarList;
